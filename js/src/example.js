eb9f1109cd1ba87356c5a8a4daf1f92543db2200
Martin Sandve Alnæs <martinal@simula.no>
2017-01-24 15:47:05 +0100

    Draft initial widget model.

--- js/src/example.js
+++ js/src/example.js
@@ -1,5 +1,10 @@
 var widgets = require('jupyter-js-widgets');
 var _ = require('underscore');
+//var THREE = require('three');
+var p3js = require(["nbextensions/jupyter-threejs/index"]);
+
+
+var npm_module_name = 'juypter-volren-widget';
 
 
 // Custom Model. Custom widgets models must at least provide default values
@@ -8,31 +13,55 @@ var _ = require('underscore');
 //
 // When serialiazing entire widget state for embedding, only values different from the
 // defaults will be specified.
-var HelloModel = widgets.DOMWidgetModel.extend({
+var VolRenModel = widgets.DOMWidgetModel.extend({
     defaults: _.extend({}, widgets.DOMWidgetModel.prototype.defaults, {
-        _model_name : 'HelloModel',
-        _view_name : 'HelloView',
-        _model_module : 'juypter-volren-widget',
-        _view_module : 'juypter-volren-widget',
-        value : 'Hello World'
+        _model_name : 'VolRenModel',
+        _view_name : 'VolRenView',
+        _model_module : npm_module_name,
+        _view_module : npm_module_name,
+
+        // No rendering should happen until this is set to True
+        ready : false,
+
+        // Shaders
+        vertex_shader : undefined,
+        fragment_shader : undefined,
+
+        // Geometry
+        coordinates : undefined,
+        triangles : undefined,
+
+        // Uniforms
+        f_min : 0.0,
+        f_max : 1.0,
+
+        // Attributes
+        f_front : undefined,
+        f_back : undefined,
+        s_front : undefined,
+        s_back : undefined,
     })
 });
 
 
 // Custom View. Renders the widget model.
-var HelloView = widgets.DOMWidgetView.extend({
+var VolRenView = widgets.DOMWidgetView.extend({
+    ready : false,
+
     render: function() {
-        this.value_changed();
-        this.model.on('change:value', this.value_changed, this);
+        this.model.on('change:ready', this.ready_changed, this);
     },
 
-    value_changed: function() {
-        this.el.textContent = this.model.get('value');
+    ready_changed: function() {
+        var ready = this.model.get('ready');
+        var f_min = this.model.get('f_min');
+        var f_max = this.model.get('f_max');
+        this.el.textContent = "f in range [" + f_min + ", " + f_max + "], ready = " + ready;
     }
 });
 
 
 module.exports = {
-    HelloModel : HelloModel,
-    HelloView : HelloView
+    VolRenModel : VolRenModel,
+    VolRenView : VolRenView
 };
--- volrenwidget/example.py
+++ volrenwidget/example.py
@@ -1,12 +1,50 @@
 import ipywidgets as widgets
-from traitlets import Unicode
+from traitlets import Unicode, Integer, Float, Bytes
 
 
-@widgets.register('hello.Hello')
-class HelloWorld(widgets.DOMWidget):
-    """"""
-    _view_name = Unicode('HelloView').tag(sync=True)
-    _model_name = Unicode('HelloModel').tag(sync=True)
-    _view_module = Unicode('juypter-volren-widget').tag(sync=True)
-    _model_module = Unicode('juypter-volren-widget').tag(sync=True)
-    value = Unicode('Hello World!').tag(sync=True)
+npm_module_name = 'juypter-volren-widget'
+
+
+# TODO: Initially use a robust version of Array with to_json
+# TODO: Later optimize using views properly
+Float32Buffer = Bytes
+Int32Buffer = Bytes
+
+
+@widgets.register('volrenwidget.VolRenWidget')
+class VolRenWidget(widgets.DOMWidget):  # TODO: Use pythreejs and make this a Widget instead
+    """A prototype volume rendering widget."""
+    _view_name = Unicode('VolRenView').tag(sync=True)
+    _model_name = Unicode('VolRenModel').tag(sync=True)
+    _view_module = Unicode(npm_module_name).tag(sync=True)
+    _model_module = Unicode(npm_module_name).tag(sync=True)
+
+    # No rendering should happen until this is set to True
+    ready = Bool(False).tag(sync=True)
+
+    # Shaders
+    vertex_shader = Unicode().tag(sync=True)
+    fragment_shader = Unicode().tag(sync=True)
+
+    # Geometry
+    coordinates = Float32Buffer().tag(sync=True)
+    triangles = Int32Buffer().tag(sync=True)
+
+    # Uniforms
+    # TODO: Allow fully customized uniforms dict?
+    #uniforms = Dict().tag(sync=True)
+    f_min = Float(0.0).tag(sync=True)
+    f_max = Float(1.0).tag(sync=True)
+
+    # Attributes
+    # TODO: Allow fully customized attributes dict?
+    #attributes = Dict().tag(sync=True)
+    f_front = Float32Buffer().tag(sync=True)
+    f_back = Float32Buffer().tag(sync=True)
+    s_front = Float32Buffer().tag(sync=True)
+    s_back = Float32Buffer().tag(sync=True)
+
+    # TODO: Higher level interface for setting things in bulk
+    def set_foo(self, *args):
+        # TODO: Hold and update several at once
+        pass
